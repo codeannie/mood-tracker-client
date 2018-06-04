9779af8b55481a242e2464e93c45871a948eb4d9
Annie Chiang <devbear22@gmail.com>
2018-06-03 20:34:19 -0700

    add routing for signup/login forms

--- src/landing-page/landing-page.component.js
+++ src/landing-page/landing-page.component.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import LandingContent from './landing-content.component';
 import SignUpForm from './signupForm-component';
 import LoginForm from './loginForm-component';
@@ -7,11 +8,28 @@ import './landing-page.css';
 export default class LandingPage extends React.Component {
   render() {
     return (
-      <div className="landing-page">
-        <LandingContent />
-        <SignUpForm />
-        <LoginForm />
-      </div>
+      <Router>
+        <div className="landing-page">
+          <div className="nav-buttons">
+            <ul>
+              <li>
+                <Link to="/"> Home </Link>
+              </li>
+              <li>
+                <Link to="signup"> Sign Up </Link>
+              </li>
+              <li>
+                <Link to="login"> Login </Link>
+              </li>
+            </ul>
+          </div>
+
+          <Route exact path="/" component={LandingContent} />
+          <Route exact path="/signup" component={SignUpForm} />
+          <Route path="/login" component={LoginForm} />
+          
+        </div>
+      </Router>
     );
   }
 }
