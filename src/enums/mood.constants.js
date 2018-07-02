27d6dfe04fc91fc72597b72835a8936f024db4f5
Annie Chiang <devbear22@gmail.com>
2018-07-01 18:21:28 -0700

    add mood constants

---
+++ src/enums/mood.constants.js
@@ -0,0 +1,36 @@
+const MOODS = {
+  NORMAL: {
+    id: 0,
+    label: 'Normal/Average',
+  },
+  HAPPY: {
+    id: 1,
+    label: 'Happy/Joyful',
+  },
+  PRODUCTIVE: {
+    id: 2,
+    label: 'Productive/Energetic',
+  },
+  ANNOYED: {
+    id: 3,
+    label: 'Annoyed',
+  },
+  SAD: {
+    id: 4,
+    label: 'Sad/Meloncholy',
+  },
+  STRESSED: {
+    id: 5,
+    label: 'Anxious/Stressed',
+  },
+  SICK: {
+    id: 6,
+    label: 'Sick/Tired',
+  },
+  ANGRY: {
+    id: 7,
+    label: 'Angry/Grumpy',
+  },
+};
+
+export default MOODS;
