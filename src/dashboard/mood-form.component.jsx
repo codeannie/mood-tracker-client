2009b47f9e9521a71ef924ab32841388b96d2945
Annie Chiang <devbear22@gmail.com>
2018-08-05 17:14:16 -0700

    added mood form container & commented out value in form for now

--- src/dashboard/mood-form.component.jsx
+++ src/dashboard/mood-form.component.jsx
@@ -31,31 +31,34 @@ export default class MoodForm extends React.Component {
 
   render() {
     return (
-      <form className="moodForm" onSubmit={this.handleSubmit}>
-        <label htmlFor="mood">
+      <div className="moodForm-container">
+        <form className="moodForm" onSubmit={this.handleSubmit}>
+          <label htmlFor="mood">
           Current Mood:
-          <select value={this.state.mood} onChange={this.handleChange}>
-            <option value={MOODS.NORMAL.id} > {MOODS.NORMAL.label} </option>
-            <option value={MOODS.HAPPY.id} > {MOODS.HAPPY.label} </option>
-            <option value={MOODS.PRODUCTIVE.id} > {MOODS.PRODUCTIVE.label} </option>
-            <option value={MOODS.ANNOYED.id} > {MOODS.ANNOYED.label} </option>
-            <option value={MOODS.SAD.id} > {MOODS.SAD.label} </option>
-            <option value={MOODS.STRESSED.id} > {MOODS.STRESSED.label} </option>
-            <option value={MOODS.SICK.id} > {MOODS.SICK.label} </option>
-            <option value={MOODS.ANGRY.id} > {MOODS.ANGRY.label} </option>
-          </select>
+            <select onChange={this.handleChange}>
+              {/* <select value={this.state.mood} onChange={this.handleChange}> */}
+              <option value={MOODS.NORMAL.id} > {MOODS.NORMAL.label} </option>
+              <option value={MOODS.HAPPY.id} > {MOODS.HAPPY.label} </option>
+              <option value={MOODS.PRODUCTIVE.id} > {MOODS.PRODUCTIVE.label} </option>
+              <option value={MOODS.ANNOYED.id} > {MOODS.ANNOYED.label} </option>
+              <option value={MOODS.SAD.id} > {MOODS.SAD.label} </option>
+              <option value={MOODS.STRESSED.id} > {MOODS.STRESSED.label} </option>
+              <option value={MOODS.SICK.id} > {MOODS.SICK.label} </option>
+              <option value={MOODS.ANGRY.id} > {MOODS.ANGRY.label} </option>
+            </select>
 
-        </label>
-        <label htmlFor="notes">
-          <textarea 
-            value={this.state.notes}
-            onChange={this.handleChange} 
-          />
-        </label>
+          </label>
+          <label htmlFor="notes">
+            <textarea 
+              // value={this.state.notes}
+              onChange={this.handleChange} 
+            />
+          </label>
 
-        <button type="submit"> Add Mood </button>
-        <button> Cancel </button>
-      </form> 
+          <button type="submit"> Add Mood </button>
+          <button> Cancel </button>
+        </form>
+      </div>
     );
   }
 }
