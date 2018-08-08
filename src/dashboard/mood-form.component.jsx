97dab41774b03cc09f3920ecc089167978ad63a8
Annie Chiang <devbear22@gmail.com>
2018-08-07 22:39:37 -0700

    attempt to streamline generation of mood options; need to add loop

--- src/dashboard/mood-form.component.jsx
+++ src/dashboard/mood-form.component.jsx
@@ -4,27 +4,43 @@ import MOODS from '../enums/mood.constants';
 export default class MoodForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      mood: '',
+    this.state = {   
+      // capture selected mood?
+      mood: {
+        id: '',
+        label: '',
+      },
       notes: '',
     };
     
-    this.handleChange = this.handleChange.bind(this);
+    this.generateMoodOptions = this.generateMoodOptions.bind(this);
+    this.handleChange = this.handleMoodChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(e) {
+  generateMoodOptions() {
+    const moodOptions = [];
+    const moodKeys = Object.keys(MOODS);
+    console.log('mood keys ->', moodKeys);
+    // need to loop through object to generate list of options
+    const moodOption = <option data-name={moodKeys.label} value={moodKeys.id}> {moodKeys.label} </option>;
+    moodOptions.push(moodOption);
+  }
+
+  handleMoodChange(e) {
     const target = e.target;
     const value = target.value;
     const name = target.name;
 
     this.setState(({
+      // store the mood or notes 
       [name]: value,
     }));
 
     console.log('state on change ->', this.state);
   }
 
+  // add separate handlechange for text area
   handleSubmit(e) {
     e.preventDefault();
     // do something to sent to API
@@ -37,23 +53,23 @@ export default class MoodForm extends React.Component {
         <form className="moodForm" onSubmit={this.handleSubmit}>
           <label htmlFor="mood">
           Current Mood:
-            {/* <select onChange={this.handleChange}> */}
-            <select value={this.state.mood} onChange={this.handleChange}>
-              <option value={MOODS.NORMAL.id} > {MOODS.NORMAL.label} </option>
+            <select value={this.state.mood} onChange={this.handleMoodChange}>
+              {this.generateMoodOptions}
+              {/* <option value={MOODS.NORMAL.id} > {MOODS.NORMAL.label} </option>
               <option value={MOODS.HAPPY.id} > {MOODS.HAPPY.label} </option>
               <option value={MOODS.PRODUCTIVE.id} > {MOODS.PRODUCTIVE.label} </option>
               <option value={MOODS.ANNOYED.id} > {MOODS.ANNOYED.label} </option>
               <option value={MOODS.SAD.id} > {MOODS.SAD.label} </option>
               <option value={MOODS.STRESSED.id} > {MOODS.STRESSED.label} </option>
               <option value={MOODS.SICK.id} > {MOODS.SICK.label} </option>
-              <option value={MOODS.ANGRY.id} > {MOODS.ANGRY.label} </option>
+              <option value={MOODS.ANGRY.id} > {MOODS.ANGRY.label} </option> */}
             </select>
 
           </label>
           <label htmlFor="notes">
             <textarea 
               value={this.state.notes}
-              onChange={this.handleChange} 
+              // onChange={this.handleChange} 
             />
           </label>
 
